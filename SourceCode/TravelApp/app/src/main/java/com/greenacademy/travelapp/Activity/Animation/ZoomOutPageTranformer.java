b2210f990c0c879f4d0d6a073ecd6814ade72520
WNXFIZQQU9APQGR\Administrator <tranminh.hoanglong0612@gmail.com>
2017-01-14 21:13:15 +0700

    Final Tutorial + Splash Screen

--- SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Animation/ZoomOutPageTranformer.java
+++ SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Animation/ZoomOutPageTranformer.java
@@ -8,8 +8,8 @@ import android.view.View;
  */
 
 public class ZoomOutPageTranformer implements ViewPager.PageTransformer {
-    private static final float MIN_SCALE = 0.85f;
-    private static final float MIN_ALPHA = 0.5f;
+    private static final float MIN_SCALE = 1f;
+    private static final float MIN_ALPHA = 1f;
 
     @Override
     public void transformPage(View view, float position) {
