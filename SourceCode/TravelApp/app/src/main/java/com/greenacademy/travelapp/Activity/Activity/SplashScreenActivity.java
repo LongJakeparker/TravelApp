268acbbc0cfd1957af755b2ee4cca13ad55a352c
WNXFIZQQU9APQGR\Administrator <tranminh.hoanglong0612@gmail.com>
2017-01-14 21:04:25 +0700

    code xong

--- SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Activity/SplashScreenActivity.java
+++ SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Activity/SplashScreenActivity.java
@@ -7,32 +7,31 @@ import android.os.Bundle;
 
 import com.greenacademy.travelapp.Activity.Constant.Constant;
 import com.greenacademy.travelapp.Activity.MainActivity;
-import com.greenacademy.travelapp.Activity.Utils.MySharedPreferences;
 import com.greenacademy.travelapp.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    public static int SPLASH_TIME_OUT = 2000;
+    public static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        final MySharedPreferences mySharedPreferences = new MySharedPreferences(SplashScreenActivity.this, Constant.Check_First_Time);
-
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try{
                     Thread.sleep(SPLASH_TIME_OUT);
-                    if (mySharedPreferences.getString(Constant.Check_First_Time) == null){
-                        mySharedPreferences.putString(Constant.Check_First_Time,Constant.Status_Check_False);
-                        Intent intent = new Intent(SplashScreenActivity.this, TutorialActivity.class);
-                        startActivity(intent);
-                    }else if(mySharedPreferences.getString(Constant.Check_First_Time) == Constant.Status_Check_False){
-                        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                        startActivity(intent);
-                    }
+                    Intent intent = new Intent(SplashScreenActivity.this, TutorialActivity.class);
+                    startActivity(intent);
+//                    if (mySharedPreferences.getString(Constant.Check_First_Time) == null){
+//                        mySharedPreferences.putString(Constant.Check_First_Time,Constant.Status_Check_False);
+//                        Intent intent = new Intent(SplashScreenActivity.this, TutorialActivity.class);
+//                        startActivity(intent);
+//                    }else if(mySharedPreferences.getString(Constant.Check_First_Time) == Constant.Status_Check_False){
+//                        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
