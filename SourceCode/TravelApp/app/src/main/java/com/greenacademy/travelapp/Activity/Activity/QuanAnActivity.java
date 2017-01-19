4e8a408b369e40f2f30d4e1d55484c5eee87804c
vison <gnvisons@gmail.com>
2017-01-19 21:12:13 +0700

    màn hình quán ăn p.4. Xong giao diện, dữ liệu thô

--- SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Activity/QuanAnActivity.java
+++ SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Activity/QuanAnActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import com.greenacademy.travelapp.Activity.Adapter.CustomLayoutManager;
 import com.greenacademy.travelapp.Activity.Adapter.LoaiQuanAnAdapter;
 import com.greenacademy.travelapp.Activity.Adapter.QuanGanToiAdapter;
+import com.greenacademy.travelapp.Activity.Adapter.TopCheckinAdapter;
 import com.greenacademy.travelapp.Activity.Model.LoaiQuanAn;
 import com.greenacademy.travelapp.Activity.Model.QuanGanToi;
 import com.greenacademy.travelapp.R;
@@ -17,9 +19,11 @@ import java.util.ArrayList;
 public class QuanAnActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    RecyclerView recyLoaiQuan, recyQuanGanToi;
+    RecyclerView recyLoaiQuan, recyQuanGanToi, recyTopCheckin;
     LoaiQuanAnAdapter adapterLoaiQuan;
     QuanGanToiAdapter adapterQuanGanToi;
+    TopCheckinAdapter adapterTopCheckin;
+    CustomLayoutManager customLayoutManager;
     ArrayList<LoaiQuanAn> listImageLoaiQuan;
     ArrayList<QuanGanToi> listImageQuanGanToi;
 
@@ -30,13 +34,32 @@ public class QuanAnActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         recyLoaiQuan = (RecyclerView) findViewById(R.id.recyclerViewLoaiQuan);
         recyQuanGanToi = (RecyclerView) findViewById(R.id.recyclerViewQuanGanToi);
+        recyTopCheckin = (RecyclerView) findViewById(R.id.recyclerViewTopCheckin);
+        customLayoutManager = new CustomLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         listImageLoaiQuan = new ArrayList<LoaiQuanAn>();
         listImageQuanGanToi = new ArrayList<QuanGanToi>();
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // phần này dùng dữ liệu giả
         listImageLoaiQuan.add(new LoaiQuanAn(R.drawable.test));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.drawable.test));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
+        listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
         listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
         listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
         listImageLoaiQuan.add(new LoaiQuanAn(R.mipmap.ic_launcher));
@@ -48,6 +71,7 @@ public class QuanAnActivity extends AppCompatActivity {
 
         adapterLoaiQuan = new LoaiQuanAnAdapter(listImageLoaiQuan);
         adapterQuanGanToi = new QuanGanToiAdapter(listImageQuanGanToi);
+        adapterTopCheckin = new TopCheckinAdapter(listImageLoaiQuan);
 
         LinearLayoutManager layoutManagerLoaiQuan = new LinearLayoutManager(getApplicationContext());
         layoutManagerLoaiQuan.setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -63,5 +87,7 @@ public class QuanAnActivity extends AppCompatActivity {
 //        layoutManager = new CustomLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
 //        recyQuanGanToi.setLayoutManager(layoutManager);
 //        recyQuanGanToi.setAdapter(adapterQuanGanToi);
+        recyTopCheckin.setLayoutManager(customLayoutManager);
+        recyTopCheckin.setAdapter(adapterTopCheckin);
     }
 }
