8ec7c8cbccd732e8f2b596b42c496a5176ec9b8e
DESKTOP-242LDGV\PhamNhuVu <phamnhuvu.dev@gmail.com>
2017-02-14 20:07:43 +0700

    no message

--- SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Fragment/ChiTietDiaDiemFragment.java
+++ SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Fragment/ChiTietDiaDiemFragment.java
@@ -7,23 +7,36 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.greenacademy.travelapp.R;
 
 
 public class ChiTietDiaDiemFragment extends Fragment {
     View view;
-    RecyclerView recyclerImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_chi_tiet_dia_diem, container, false);
+        initView();
         return view;
     }
 
+    RecyclerView recyclerImage;
+    ImageView imageDiaDiem;
+    TextView numberLike, numberView, numberReview;
+
     private void initView(){
         recyclerImage = (RecyclerView) view.findViewById(R.id.recyclerImage);
+        imageDiaDiem = (ImageView) view.findViewById(R.id.image_ChiTietDiaDiem);
+
+        numberLike = (TextView) view.findViewById(R.id.numberLike);
+        numberView = (TextView) view.findViewById(R.id.numberReview);
+        numberReview = (TextView) view.findViewById(R.id.numberReview);
+
     }
 
 }
---
+++ SourceCode/TravelApp/app/src/main/java/com/greenacademy/travelapp/Activity/Fragment/DuLichFragment.java
@@ -0,0 +1,25 @@
+package com.greenacademy.travelapp.Activity.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.greenacademy.travelapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DuLichFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_du_lich, container, false);
+    }
+
+}
